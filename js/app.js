cbfc55c84c7d5bbb13e1c728f4aecd4ce532d3a1
MohdAzzam <alazzamhmode@gmail.com>
2021-03-23 11:16:36 +0200

    fixing the selected

--- js/app.js
+++ js/app.js
@@ -1,33 +1,47 @@
 'use strict';
+let AllKey = [];
 function AnimalGallery(title, image_url, keyword, description, horns) {
     this.title = title;
     this.image_url = image_url;
     this.keyword = keyword;
     this.description = description;
     this.horns = horns;
+    AllKey.push(this.keyword);
 }
-
+let filterArray = [];
 AnimalGallery.prototype.render = function () {
     let animal = $('#photo-template').clone();
     $('main').append(animal);
-    $('select').append(`<option value='${this.keyword}'>${this.keyword}</option>`);
+
     animal.find('h2').text(this.title);
     animal.find('img').attr('src', this.image_url);
     animal.find('img').attr('alt', this.description);
     animal.find('p').text(this.description);
     animal.removeAttr('id');
 };
+// filterArray = AllKey.filter((item, index) =>AllKey.indexOf(item) === index);
+AnimalGallery.prototype.showSelectedBox = function () {
+    filterArray.push(AllKey.filter((item, index) => AllKey.indexOf(item) === index));
+    console.log(filterArray[19]);
+    $('select option').remove();
+    if (filterArray[19] !== undefined) {
+        $('select').append(`<option value='default'>Filter by Keyword</option>`);
+        for (let i = 0; i < filterArray[19].length; i++) {
+            $('select').append(`<option value='${filterArray[19][i]}'>${filterArray[19][i]}</option>`);
+        }
+    }
+};
 
 $('select').change(function () {
     let keys = $('select option:selected').val();
     if (keys === 'default') {
         // $('section').remove();
-        $('section').attr('id','photo-template');
+        $('section').attr('id', 'photo-template');
         getAnimalData();
     } else {
         // $('section').remove();
         console.log('sssssssss');
-        $('section').attr('id','photo-template');
+        $('section').attr('id', 'photo-template');
         getAnimalDataByKey(keys);
     }
 });
@@ -40,9 +54,10 @@ function getAnimalDataByKey(key) {
     $.ajax('data/page-1.json', ajaxSetting).then(data => {
         let animalObj;
         data.forEach(item => {
-            if(item.keyword===key){
+            if (item.keyword === key) {
                 animalObj = new AnimalGallery(item.title, item.image_url, item.keyword, item.description, item.horns);
                 // console.log(animalObj);
+                animalObj.showSelectedBox();
                 animalObj.render();
             }
 
@@ -57,6 +72,7 @@ function getAnimalData() {
     $.ajax('data/page-1.json', ajaxSetting).then(data => {
         data.forEach(item => {
             let animalObj = new AnimalGallery(item.title, item.image_url, item.keyword, item.description, item.horns);
+            animalObj.showSelectedBox();
             animalObj.render();
         });
     });
