56f331d42499dd1b14d49267786c2552ebb42c17
MohdAzzam <alazzamhmode@gmail.com>
2021-03-23 21:04:22 +0200

    finish the js side

--- js/app.js
+++ js/app.js
@@ -7,73 +7,127 @@ function AnimalGallery(title, image_url, keyword, description, horns) {
     this.description = description;
     this.horns = horns;
 }
-
+// function AnimalGallery(dataObj){
+//     for(let key in dataObj){
+//         this[key]=dataObj[key];
+//     }
+// }
 AnimalGallery.prototype.render = function () {
-    let animal = $('#photo-template').clone();
+    let template = $('#mustache-template').html();
+    let html = Mustache.render(template, this);
+    $('main').append(html);
     let selectop = $('<option></option>').text(this.keyword);
     selectop.attr('value', this.keyword);
-
+    $('header select').attr('id', 'title');
     if (!(newArr.includes(this.keyword))) {
         newArr.push(this.keyword);
-        $('select').append(selectop);
+        $('#title').append(selectop);
     }
-    $('main').append(animal);
-    animal.find('h2').text(this.title);
-    animal.find('img').attr('src', this.image_url);
-    animal.find('img').attr('alt', this.description);
-    animal.find('p').text(this.description);
-    animal.removeAttr('id');
 };
 
-AnimalGallery.prototype.renderSelected = function () {
-    let animal = $('#photo-template').clone();
-    $('main').append(animal);
-    animal.find('h2').text(this.title);
-    animal.find('img').attr('src', this.image_url);
-    animal.find('img').attr('alt', this.description);
-    animal.find('p').text(this.description);
-    animal.removeAttr('id');
-};
-
-
-$('select').change(function () {
+let select = $(`<select id='filter'>
+            <option value='fil'>Filter</option>   
+             <option value='alpha'>A-Z</option>   
+             <option value='horn'>Horn</option>   
+             </select>`);
+$('main').append(select);
+$('header select').change(function () {
     let keys = $('select option:selected').val();
     if (keys === 'default') {
         // $('section').remove();
+        $('.container').empty();
         $('section').attr('id', 'photo-template');
         getAnimalData();
+        getSecoundPageData();
     } else {
         // $('section').remove();
         console.log('sssssssss');
+        $('.container').empty();
         $('section').attr('id', 'photo-template');
         getAnimalDataByKey(keys);
+        getSecoundPageDataByKey(keys);
     }
 });
-function getAnimalDataByKey(key) {
+$('#filter').change(function () {
+    let keyz = $('#filter option:selected').val();
+    if (keyz === 'alpha') {
+        console.log('alpha');
+        $('section').attr('id', 'photo-template');
+        sortAnimal(keyz);
+    } else if (keyz === 'horn') {
+        $('section').attr('id', 'photo-template');
+        console.log('horn');
+        sortAnimal(keyz);
 
-    const ajaxSetting = {
-        method: 'get',
-        dataType: 'json'
-    };
+    }
+});
+
+const ajaxSetting = {
+    method: 'get',
+    dataType: 'json'
+};
+function sortAnimal(key) {
+    $('.container').empty();
+    $.ajax('data/page-1.json', ajaxSetting).then(data => {
+        if (key === 'alpha') {
+            data.sort((a, b) => {
+                return a.title.charCodeAt() - b.title.charCodeAt();
+            });
+            data.forEach(item => {
+                let animalObj = new AnimalGallery(item.title, item.image_url, item.keyword, item.description, item.horns);
+                animalObj.render();
+            });
+        } else if (key === 'horn') {
+            data.sort((a, b) => {
+                return a.horns - b.horns;
+            });
+            console.log(data);
+            data.forEach(item => {
+                let animalObj = new AnimalGallery(item.title, item.image_url, item.keyword, item.description, item.horns);
+                animalObj.render();
+            });
+        }
+    });
+}
+function getAnimalDataByKey(key) {
+    $('.container').empty();
     $.ajax('data/page-1.json', ajaxSetting).then(data => {
         let animalObj;
+        data.sort((a, b) => {
+            return a.title.charCodeAt() - b.title.charCodeAt();
+        });
         data.forEach(item => {
             if (item.keyword === key) {
                 animalObj = new AnimalGallery(item.title, item.image_url, item.keyword, item.description, item.horns);
                 // console.log(animalObj);
-                animalObj.renderSelected();
+                animalObj.render();
             }
-
         });
     });
 }
 
+function getSecoundPageDataByKey(key) {
+    $('.container').empty();
+    $.ajax('data/page-2.json', ajaxSetting).then(data => {
+        let animalObj;
+        data.sort((a, b) => {
+            return a.title.charCodeAt() - b.title.charCodeAt();
+        });
+        data.forEach(item => {
+            if (item.keyword === key) {
+                animalObj = new AnimalGallery(item.title, item.image_url, item.keyword, item.description, item.horns);
+                // console.log(animalObj);
+                animalObj.render();
+            }
+        });
+    });
+}
 function getAnimalData() {
-    const ajaxSetting = {
-        method: 'get',
-        dataType: 'json'
-    };
+    $('.container').empty();
     $.ajax('data/page-1.json', ajaxSetting).then(data => {
+        data.sort((a, b) => {
+            return a.title.charCodeAt() - b.title.charCodeAt();
+        });
         data.forEach(item => {
             let animalObj = new AnimalGallery(item.title, item.image_url, item.keyword, item.description, item.horns);
             animalObj.render();
@@ -81,5 +135,34 @@ function getAnimalData() {
     });
 }
 
-$('document').ready(getAnimalData);
+$('main').append('<input type="submit" value="Page1" id="page-1">');
+$('#page-1').on('click', function () {
+    $('section').attr('id', 'photo-template');
+    console.log('zzzzzzzzzzz');
+    getAnimalData();
+});
 
+$('main').append('<input type="submit" value="Page2" id="page-2">');
+$('#page-2').on('click', function () {
+    // $('header select').remove();
+    $('section').attr('id', 'photo-template');
+    console.log('aaaaaaaaaaa');
+    getSecoundPageData();
+});
+
+
+function getSecoundPageData() {
+    $('.container').empty();
+    $.ajax('data/page-2.json', ajaxSetting).then(data => {
+        data.sort((a, b) => {
+            return a.title.charCodeAt() - b.title.charCodeAt();
+        });
+        data.forEach(item => {
+            let animalObj = new AnimalGallery(item.title, item.image_url, item.keyword, item.description, item.horns);
+            animalObj.render();
+        });
+    });
+}
+
+getAnimalData();
+getSecoundPageData();
